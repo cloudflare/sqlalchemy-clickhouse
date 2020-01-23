56c603e9397aee7cedc5daa5603ef5bfe05aa551
aravind <aravind.karanam@gmail.com>
2020-01-23 13:55:23 +0530

    Patch for bug in limit and offset
    refer to the following issue https://github.com/cloudflare/sqlalchemy-clickhouse/issues/56

--- base.py
+++ base.py
@@ -127,12 +127,9 @@ class ClickHouseCompiler(PGCompiler):
         if select._limit_clause is not None:
             text += '\n LIMIT ' + self.process(select._limit_clause, **kw)
         if select._offset_clause is not None:
-            text = '\n LIMIT '
             if select._limit_clause is None:
-                text += self.process(sql.literal(-1))
-            else:
-                text += '0'
-            text += ',' + self.process(select._offset_clause, **kw)
+                text += '\n LIMIT 0'
+            text += ' OFFSET '+ self.process(select._offset_clause, **kw)
         return text
 
     def for_update_clause(self, select, **kw):
